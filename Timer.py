5d223fc0db0815ad24b98dd10eec9911e851df86
armanghazaryan77 <155985232+armanghazaryan77@users.noreply.github.com>
2024-01-08 18:19:31 +0400

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,20 @@
+# import time
+# def start_stopwatch():
+#     start_time = time.time()
+#     input("Stopwatch started. Press Enter to stop.")
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     print(elapsed_time)
+# print("Welcome to the Python Stopwatch! ")
+# while True:
+#     print("\nOptions:")
+#     print("1. Start stopwatch")
+#     print("2. Exit")
+#     choice = input("Enter your choice (1-2):")
+#     if choice == '1':
+#         print(f"Elapsed Time: {start_stopwatch()}")
+#     elif choice == '2':
+#         print("Exiting the stopwatch. Goodbye!")
+#         break
+#     else:
+#         print("Invalid choice. Please enter 1 or 2.")
